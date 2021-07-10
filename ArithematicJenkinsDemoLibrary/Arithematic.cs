9778ed858d6b55c271436786af94972a7ce0c115
Dileep <dilipmbhat@gmail.com>
2021-07-10 09:50:17 -0700

    Test Jenkins trigger

--- ArithematicJenkinsDemoLibrary/Arithematic.cs
+++ ArithematicJenkinsDemoLibrary/Arithematic.cs
@@ -7,11 +7,11 @@ namespace ArithematicJenkinsDemoLibrary
         public int Add(int a, int b)
         {
             return (a - b);
-        }
+        }-
 
         public int Subtract(int a, int b)
         {
-            return (a + b);
+            return (a - b);
         }
     }
 }
