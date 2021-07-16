f608f55a38841766c26483c31003da5860884f96
Dileep <dilipmbhat@gmail.com>
2021-07-16 10:59:48 -0700

    Correct logic for Jenkins test

--- ArithematicJenkinsDemoLibrary/Arithematic.cs
+++ ArithematicJenkinsDemoLibrary/Arithematic.cs
@@ -6,12 +6,12 @@ namespace ArithematicJenkinsDemoLibrary
     {
         public int Add(int a, int b)
         {
-            return (a - b);
+            return (a + b);
         }
 
         public int Subtract(int a, int b)
         {
-            return (a + b);
+            return (a - b);
         }
     }
 }
