0354112de392cdd6fec10d2726ac4c7f5847b7b5
Dileep <dilipmbhat@gmail.com>
2021-07-16 10:56:35 -0700

    Remove invalid token

--- ArithematicJenkinsDemoLibrary/Arithematic.cs
+++ ArithematicJenkinsDemoLibrary/Arithematic.cs
@@ -7,7 +7,7 @@ namespace ArithematicJenkinsDemoLibrary
         public int Add(int a, int b)
         {
             return (a - b);
-        }-
+        }
 
         public int Subtract(int a, int b)
         {
